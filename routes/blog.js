ccc1b8c98c77e531eadaac8c130115564703ae4d
Mohit-code-dot <mohit@workinxdigital.us>
2024-07-15 14:35:03 +0530

    add

--- routes/blog.js
+++ routes/blog.js
@@ -1,17 +1,12 @@
 const express = require("express");
-const path = require("path");
 const router = express.Router();
 const blogs = require("../data/blogs");
-const { title } = require("process");
 
 router.get("/",(req,res)=>{
-    // res.sendFile(path.join(__dirname,"../template/index.html"));
     res.render("home");
 });
 
 router.get("/blog",(req,res)=>{
-    // res.sendFile(path.join(__dirname,"../views/blogHome.html"));
-
     res.render("blogHome",{
         blogs:blogs
     });
@@ -19,8 +14,6 @@ router.get("/blog",(req,res)=>{
 
 router.get("/blogpost/:title",(req,res)=>{
     myblog = blogs.filter((item)=>item.title == req.params.title)
-    // console.log(myblog);
-    // res.sendFile(path.join(__dirname,"../views/blogpage.html"));
 
     res.render('blogPage',{
         title: myblog[0].title,
